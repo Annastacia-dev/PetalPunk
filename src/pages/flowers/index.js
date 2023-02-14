76ec5d4c6c465b3ee53315c71c2b0dd5becae576
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-02-14 21:18:39 +0300

    add flowers.json

--- src/pages/flowers/index.js
+++ src/pages/flowers/index.js
@@ -3,11 +3,11 @@ import Link from 'next/link'
 
 const Flowers = ({ flowers }) => {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 mt-32 ">
+    <section className="grid grid-cols-3 md:grid-cols-2 lg:grid-cols-5  mt-32 ">
       {flowers.map((flower) => (
         <Link key={flower.id} className="flex  flex-col justify-center items-center p-4" href={`/flowers/${flower.name}`}>
-          <Image src={flower.image} alt={flower.name} width={200} height={300}
-          className="rounded-lg shadow-lg w-40 h-40 object-cover"
+          <Image src={flower.image} alt={flower.name} width={200} height={200}
+          className="rounded-lg shadow-lg sm:w-40 sm:h-40 object-cover"
            />
           <h2 className="text-md text-center mt-3">{flower.name}
             <span className="text-sm ml-3 text-gray-500">${flower.price}</span>
