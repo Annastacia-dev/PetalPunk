b09e1dfc069bdb990a463ffa34767a3cb644d0c8
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-02-16 19:33:44 +0300

    cart page

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart'
 
 const Cart = () => {
+    const { cartItems, removeFromCart, addToCart, cartTotal } = useContext(CartContext)
   return (
-    <div>Cart</div>
+    // display cart items
+    <div className="flex flex-col mt-32 justify-center items-center">
+        <h1 className="text-2xl font-bold mt-3">Cart</h1>
+        <div className="flex flex-col justify-center items-center mt-10">
+            {cartItems.map((item) => (
+                <div key={item.id} className="flex flex-row justify-center items-center mt-3">
+                    <button className="bg-rose-500 text-white p-2 rounded-md"
+                    onClick={() => removeFromCart(item)}
+                    >-</button>
+                    <p className="text-rose-500 text-lg font-bold mx-2">
+                        {item.quantity}
+                    </p>
+                    <button className="bg-rose-500 text-white p-2 rounded-md"
+                    onClick={() => addToCart(item)}
+                    >+</button>
+                    <p className="text-rose-500 text-lg font-bold mx-2">
+                        {item.name}
+                    </p>
+                    <p className="text-rose-500 text-lg font-bold mx-2">
+                        ${item.price}
+                    </p>
+                </div>
+            ))}
+        </div>
+        <div className="flex flex-col justify-center items-center mt-10">
+            <p className="text-rose-500 text-lg font-bold mx-2">
+                Total: ${cartTotal}
+            </p>
+        </div>
+    </div>
+
   )
 }
 
